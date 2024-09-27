bb39b1ff102c58a1f2595da8840ac914a3a91725
Samuelfox30 <samuelfoxgama@hotmail.com>
2024-09-27 18:44:12 -0300

    First_update

---
+++ Programa de captura de pacotes/v1.py
@@ -0,0 +1,13 @@
+from scapy.all import sniff
+
+def processa_pacote(pacote):
+    if pacote.haslayer('IP'):
+        ip_origem = pacote['IP'].src
+        ip_destino = pacote['IP'].dst
+        protocolo = pacote.proto
+        tamanho_pacote = len(pacote)
+
+        print(f'Pacote: ip_origem: {ip_origem}, ip_destino: {ip_destino}, protocolo: {protocolo}, tamanho: {tamanho_pacote}')
+
+""" sniff(prn=processa_pacote, filter="ip", store=0) """
+sniff(prn=processa_pacote, filter="ip", store=0, iface="Wi-Fi")
