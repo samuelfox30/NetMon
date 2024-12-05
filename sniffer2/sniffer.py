88290467c59b2bfb86273f73641eccee2bbd7f93
samuelfox30 <Shinininho3006>
2024-12-05 20:38:57 -0300

    cu

--- sniffer2/sniffer.py
+++ sniffer2/sniffer.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'chave_secreta'
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return "Servidor On"
-
 @socketio.on('connect')
 def on_connect():
     print('Cliente conectado.')
@@ -21,6 +17,11 @@ def on_disconnect():
 def handle_message(msg):
     print(f'Mensagem recebida: {msg}')
     socketio.send(f'Recebi a mensagem: {msg}')
+
+@socketio.on('numero')
+def handle_message(msg):
+    print(f'Numero recebido: {msg}')
+    socketio.send(f'Recebi o numero: {msg}')
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
